d8282c2227fd7cf4daeac7e6bb109083a5951ca5
joaks <joaquinsepulvedaraya@gmail.com>
2016-03-21 11:45:29 -0300

    finalizacion del taller

---
+++ app/controllers/pags_controller.rb
@@ -0,0 +1,22 @@
+class PagsController < ApplicationController
+ 	
+  before_action :get_data
+
+  def pag1
+  	render layout: "landing"
+  end
+
+  def pag2
+  end
+
+  def pag3
+  	
+  end	
+
+  private
+
+ 	def get_data
+ 		 @users = ["Alumno1", "Alumno2"]
+ 	end
+
+end
